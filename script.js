1b03dbbeb12e0407b630028f0e9611ff7cdfb201
o0ops <mariantdm@icloud.com>
2023-02-28 18:13:32 +0000

    added the html structure

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+// select elements
+const displayBooks = document.querySelector('.myLibrary');
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -12,10 +13,15 @@ function Book(title, author, pages, read) {
 let book1 = new Book('ahile', 'morar', 201, true);
 // add the new book to the myLibrary array
 function addBookToLibrary() {
-  myLibrary.push(book1);
+  let title = prompt('what is the title?');
+  let author = prompt('what is the author?');
+  let pages = prompt('how many pages?');
+  let read = prompt('Have you read it?');
+  let result = new Book(title, author, pages, read);
+  myLibrary.push(result);
 }
 //call addBookToLibrary
-addBookToLibrary();
+// addBookToLibrary();
 //
 myLibrary.forEach(element => {
   let div = document.createElement('div');
