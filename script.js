b078645c48c02bfbf7b678bd1e6251b220ebc454
o0ops <mariantdm@icloud.com>
2023-03-07 13:26:33 +0000

    changed the function display()

--- script.js
+++ script.js
@@ -11,24 +11,26 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
-// create a new book
-let book1 = new Book('ahile', 'morar', 201, true);
-// add the new book to the myLibrary array
 function addBookToLibrary() {
-  let title = prompt('what is the title?');
-  let author = prompt('what is the author?');
-  let pages = prompt('how many pages?');
-  let read = prompt('Have you read it?');
-  let result = new Book(title, author, pages, read);
-  myLibrary.push(result);
+  addBookBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').value;
+    let result = new Book(title, author, pages, read);
+    myLibrary.push(result);
+    display();
+  });
 }
 //call addBookToLibrary
-// addBookToLibrary();
+addBookToLibrary();
 //
 function display() {
   myLibrary.forEach(element => {
     let div = document.createElement('div');
     div.classList.add('all-books');
+    div.innerHTML = '';
     div.innerHTML = `${element.title} ${element.author} ${element.pages} ${element.read}`;
     document.body.appendChild(div);
   });
